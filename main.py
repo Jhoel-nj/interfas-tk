8580e0eb8763ee255cd5f2add4428784ff8fa035
jhoel-nj <167356954+Jhoel-nj@users.noreply.github.com>
2024-10-04 13:57:25 -0400

    se crea el file img con los logos y se añaden lineas basicas sobre tkinter

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from tkinter import *
-
 #que es un Frame?
 
-root = Tk()         #crear
-root.mainloop()     #ventana
 
+root = Tk()         #crear ventana
+
+root.title('Dota 2')  #anadir titulo a mi ventana
+
+root.resizable(1,1) #perimite ampliar la ventana segun sus parametros(1,1)(1,0)(0,0)(0,1)
+
+root.iconbitmap(r"img/logo32.ico")
+
+root.geometry('650x350')    #tamano inicial de la ventana
+
+root.config(bg='green')     #color del fondo de la ventana
+
+root.mainloop()
